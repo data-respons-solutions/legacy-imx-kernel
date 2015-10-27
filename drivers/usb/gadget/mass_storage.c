9d693e5e6aeee7a5bbc63fd6e4b72d66da4ccd53
Hans Christian Lonstad <hcl@datarespons.no>
2015-10-27 20:36:04 +0100

    LVSCOREI-55: Implemented SPDR 141

--- drivers/usb/gadget/mass_storage.c
+++ drivers/usb/gadget/mass_storage.c
@@ -148,7 +148,11 @@ put_func:
 }
 
 static struct usb_configuration msg_config_driver = {
+#ifdef CONFIG_USB_MASS_STORAGE_LABEL
+	.label = CONFIG_USB_MASS_STORAGE_LABEL,
+#else
 	.label			= "Linux File-Backed Storage",
+#endif
 	.bConfigurationValue	= 1,
 	.bmAttributes		= USB_CONFIG_ATT_SELFPOWER,
 };
