02b66b342d12f0b161f0a8cf15f56bdc61ca08d5
Hans Christian Lonstad <hcl@datarespons.no>
2017-11-30 11:25:34 +0100

    SPDR-393: fixed
    Corrected capitalization

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -235,7 +235,7 @@ static const char fsg_string_interface[] = "Mass Storage";
 
  #ifdef CONFIG_USB_MASS_STORAGE_LAERDAL
  static const bool cache_off = true;
- static const char *gadget_name = "Simpad PLUS";
+ static const char *gadget_name = "SimPad PLUS";
  static const char *vendor_name = "Laerdal";
  #else
  static const bool cache_off = false;
